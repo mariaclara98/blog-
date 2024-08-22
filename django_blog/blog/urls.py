4f45147cd28de249ea40b5361e6055eb603619d0
Clara <mariaclaraguillen756@gmail.com>
2024-08-21 21:05:06 -0500

    primer commit

---
+++ django_blog/blog/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path
+
+from django.conf.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include(('core.urls', 'core'), namespace='core')),
+    path('users/', include(('users.urls', 'users'), namespace='users')),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
